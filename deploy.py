d95b458ac045ceceec7fe5f83e5fa50585f294d0
Aryan Agrawal <alemantrixaryan@gmail.com>
2022-02-05 03:59:17 +0530

    contract call

--- deploy.py
+++ deploy.py
@@ -62,10 +62,26 @@ nonce = w3.eth.getTransactionCount(my_address)
 transaction = SimpleStorage.constructor().buildTransaction(
     {"gasPrice": w3.eth.gas_price, "chainId": chain_id, "from": my_address, "nonce": nonce})
 # print(transaction)
-
+private_key = config.get('PRIVATE_KEY')
 # signing my transaction with private key
 signed_txn = w3.eth.account.sign_transaction(
-    transaction, private_key=config.get('PRIVATE_KEY'))
+    transaction, private_key=private_key)
 
 # deploy the transaction
 tx_hash = w3.eth.send_raw_transaction(signed_txn.rawTransaction)
+tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
+
+# working with contract
+simple_storage = w3.eth.contract(address=tx_receipt.contractAddress, abi=abi)
+
+print(simple_storage.functions.retrieve().call())
+store_transaction = simple_storage.functions.store(143).buildTransaction(
+    {"gasPrice": w3.eth.gas_price, "chainId": chain_id, "from": my_address, "nonce": nonce + 1})
+
+signed_store_txn = w3.eth.account.sign_transaction(
+    store_transaction, private_key=private_key)
+
+send_store_tx = w3.eth.send_raw_transaction(signed_store_txn.rawTransaction)
+tx_receipt = w3.eth.wait_for_transaction_receipt(send_store_tx)
+# getting the state changed value
+print(simple_storage.functions.retrieve().call())
