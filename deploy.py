e0e3985db9c12a0d6d621b6f519af44587af8a23
Aryan Agrawal <alemantrixaryan@gmail.com>
2022-02-05 01:17:50 +0530

    bytecode added

--- deploy.py
+++ deploy.py
@@ -1,6 +1,9 @@
 # import compile_standard from solcx
 from solcx import compile_standard, install_solc
 from web3 import Web3
+import json
+import os
+from dotenv import dotenv_values
 
 with open("./SimpleStorage.sol",  'r') as f:
     simpleStorage = f.read()
@@ -21,3 +24,21 @@ compiled_sol = compile_standard(
 )
 
 # print(compiled_sol)
+
+with open("compiled_code.json", 'w') as file:
+    json.dump(compiled_sol, file)
+
+# get byte code
+with open("compiled_code.json", 'r') as a_file:
+    data = json.load(a_file)
+
+byte_code = data["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["evm"]["bytecode"]["opcodes"]
+
+print(byte_code)
+
+# # to connect to ganache
+# w3 = Web3(Web3.HTTPProvider("HTTP: // 127.0.0.1:7545"))
+# private_key = os.getenv("PRIVATE_KEY")
+
+# config = dotenv_values(".env")
+# print(config)
