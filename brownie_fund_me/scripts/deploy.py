852f14e5972d7908f4e776503141207e06c0258e
Aryan Agrawal <alemantrixaryan@gmail.com>
2022-02-10 20:50:55 +0530

    fundme deployed

--- brownie_fund_me/scripts/deploy.py
+++ brownie_fund_me/scripts/deploy.py
@@ -1,8 +1,13 @@
 from brownie import FundMe
+from scripts.helpful_scripts import get_account
 
 
 def deploy_fund_me():
-    pass
+    account = get_account()
+    # print(account)
+    fund = FundMe.deploy(
+        "0xCD68fB5449c5Cc1c09F430Cb90c2c0A89e8eF1Ad", {"from": account})
+    # print(f"deployed to {fund.address}")
 
 
 def main():
